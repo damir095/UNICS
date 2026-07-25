b745c5bb614672d1e8d603f7492fa235c8a0e220
damir095 <damir.mukhamadiev095@gmail.com>
2026-07-25 13:12:38 +0500

    theme_unics 0.7.44: рамка аватара + акцент плашки приветствия (мотивация этап 3)

--- theme/unics/version.php
+++ theme/unics/version.php
@@ -9,9 +9,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_unics';
-$plugin->version   = 2026071500;
+$plugin->version   = 2026072500;
 $plugin->requires  = 2025041400; // Moodle 5.0.
-$plugin->release   = '0.7.43';
+$plugin->release   = '0.7.44';
 $plugin->maturity  = MATURITY_ALPHA;
 
 $plugin->dependencies = [
