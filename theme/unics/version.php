e5fe7c03d8ba57debf7a91891f5cb29d6a389c30
damir095 <damir.mukhamadiev095@gmail.com>
2026-07-28 14:19:39 +0500

    theme_unics: темная тема - теплая шапка .unics-table побеждает .table-light на конвертированных таблицах (единый вид шапки в темной A1)

--- theme/unics/version.php
+++ theme/unics/version.php
@@ -9,9 +9,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_unics';
-$plugin->version   = 2026072701;
+$plugin->version   = 2026072702;
 $plugin->requires  = 2025041400; // Moodle 5.0.
-$plugin->release   = '0.7.47';
+$plugin->release   = '0.7.48';
 $plugin->maturity  = MATURITY_ALPHA;
 
 $plugin->dependencies = [
